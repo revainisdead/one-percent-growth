a40c8ec1f47c935d00eccb9b312b7ec8a2092f4a
Christian Hall <chall@incytestudios.com>
2021-09-03 09:41:59 -0700

    Rename new dispatches file to thunks.

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -8,41 +8,20 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 import { getSavingsFile } from "./store/selectors.js";
-import { parseSavingsFile } from "./store/dispatches.js";
-
-import { useMountEffect } from "./helpers.js";
+import { parseSavingsFile } from "./store/thunks.js";
 
 
 const App = (props) => {
-    const [fileData, setFileData] = React.useState(null);
     const [fileSet, setFileSet] = React.useState(false);
     const inputEl = React.useRef(null);
 
     const fileClick = () => document.getElementById('file-input').click();
-
-    useMountEffect(() => {
-        // TODO: Manage multiple file uploads
-
-        //const openFileElem = document.getElementById('file-input')
-        //let openFile = null;
-        //if (openFileElem !== null && openFileElem.files.length > 0) {
-        //    openFile = openFileElem.files[0]
-        //}
-
-        //setFileData(results.data);
-
-        //if (inputEl !== null) {
-        //    console.log('mount effect')
-        //    props.parseSavingsFile(inputEl.current.files);
-        //}
-    }, []);
+    const fileUploadChange = () => setFileSet(true);
 
     React.useEffect(() => {
         console.log('file set use effect', inputEl.current)
         props.parseSavingsFile(inputEl.current.files[0]);
     }, [fileSet]);
-
-    console.log('local state test', fileData);
 
     return (
         <div className="app">
@@ -53,7 +32,7 @@ const App = (props) => {
                         <input
                             id="file-input"
                             ref={inputEl}
-                            onChange={() => setFileSet(true)}
+                            onChange={fileUploadChange}
                             type="file"
                             name="name"
                             style={{"display": "none"}}
--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -1,16 +1,42 @@
+// Separate by action type, action, and action creator
+
 export const SET_KEY = "SET_KEY";
-export const setKey = (key, value) => {
-    return {
-        type: SET_KEY,
-        key: key,
-        value: value,
-    };
+// Action
+const setKeyAction = {
+    type: SET_KEY,
+    key: key,
+    value: value,
+};
+// Action creator
+export const setKey = ({key, value}) => {
+    return setKeyAction;
 };
 
 export const TOGGLE_KEY = "TOGGLE_KEY";
-export const toggleKey = (key) => {
-    return {
-        type: TOGGLE_KEY,
-        key: key,
-    };
+// Action
+const toggleKeyAction = {
+    type: TOGGLE_KEY,
+    key: key,
 };
+// Action creator
+export const toggleKey = ({key}) => {
+    return toggleKeyAction;
+};
+
+
+//export const SET_KEY = "SET_KEY";
+//export const setKey = ({key, value}) => {
+//    return {
+//        type: SET_KEY,
+//        key: key,
+//        value: value,
+//    };
+//};
+//
+//export const TOGGLE_KEY = "TOGGLE_KEY";
+//export const toggleKey = ({key}) => {
+//    return {
+//        type: TOGGLE_KEY,
+//        key: key,
+//    };
+//};

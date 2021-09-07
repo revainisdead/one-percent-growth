0820e5d352f3ce2529a366e5dd8334815c3d347e
Christian Hall <chall@incytestudios.com>
2021-09-07 07:54:18 -0700

    Begin including prettier.

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,6 +6,38 @@ set -e
 script_base=$(dirname $0)
 project_root="$script_base/.."
 
+
+
+#detach=
+#
+#POSITIONAL=()
+#while [[ $# -gt 0 ]]; do
+#    key="$1"
+#    case $key in
+#        -h|--help|?)
+#            echo "Rebuild, create, run and attach all or specfic Docker services."
+#            echo
+#            cecho "Usage: $(basename $0) <options> [SERVICE...]" default bold
+#            echo
+#            echo "Options:"
+#            echo "  -d                  Detached mode: Run containers in background."
+#            echo "  -h  --help          This help message"
+#            echo
+#            echo "Run 'docker-compose up --help' to see more options"
+#            exit 0
+#            ;;
+#        -d|--detach)
+#            detach=1
+#            ;;
+#        *)
+#            POSITIONAL+=("$key")
+#            ;;
+#    esac
+#    shift
+#done
+#set -- "${POSITIONAL[@]}"
+
+
 cd $project_root
 echo "Moved to project root: $project_root"
 source $script_base/utils.sh
